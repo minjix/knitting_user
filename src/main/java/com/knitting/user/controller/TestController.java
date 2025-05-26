157e49a7890bdbda9488a8198306097986d8efe8
minji <bmj689@ccmedia.co.kr>
2025-05-26 17:18:15 +0900

    최초 커밋

---
+++ src/main/java/com/knitting/user/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.knitting.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
