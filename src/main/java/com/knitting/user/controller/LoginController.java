16913fbf910cd80e2d3f20a255a99d4659422bd8
minji <bmj689@ccmedia.co.kr>
2025-06-09 17:08:37 +0900

    User VO 삭제 및 DTO 생성

--- src/main/java/com/knitting/user/controller/LoginController.java
+++ src/main/java/com/knitting/user/controller/LoginController.java
@@ -1,7 +1,15 @@
 package com.knitting.user.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.knitting.user.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
+@RequestMapping("/login")
 public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
 }
--- src/main/java/com/knitting/user/dto/UserDto.java
+++ src/main/java/com/knitting/user/dto/UserDto.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class UserVO {
+public class UserDto {
 
     /*유저ID*/
     private int userId;
