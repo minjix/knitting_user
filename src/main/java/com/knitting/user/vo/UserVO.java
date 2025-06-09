d660090bf2333c5a38b56deb1672245d16988ce3
minji <bmj689@ccmedia.co.kr>
2025-06-09 16:02:19 +0900

    사용자VO 생성

---
+++ src/main/java/com/knitting/user/vo/UserVO.java
@@ -0,0 +1,40 @@
+package com.knitting.user.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserVO {
+
+    /*유저ID*/
+    private int userId;
+
+    /*로그인ID*/
+    private  String loginID;
+
+    /*비밀번호*/
+    private String password;
+
+    /*생년월일*/
+    private Date birthdate;
+
+    /*권한 (예: ROLE_USER, ROLE_ADMIN)*/
+    private  String role;
+
+    /*잠금 여부 (TRUE일 경우 로그인 불가)*/
+    private boolean isLocked;
+
+    /*생성 시각*/
+    private Date createdAt;
+
+    /*수정 시각*/
+    private Date updatedAt;
+
+}
