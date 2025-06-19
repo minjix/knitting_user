1e3258ed8a44f67ae1a98430411d5ed103be8390
minjix <bminjii1014@gmail.com>
2025-06-19 14:59:34 +0900

    20250619 - 스프링 시큐리티 적용 소스 원복

--- src/main/java/com/knitting/user/mapper/RoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.knitting.user.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-@Mapper
-public interface RoleMapper {
-    Long findRoleIdByNLame(@Param("name") String name);
-}
--- src/main/java/com/knitting/user/mapper/UserRolesMapper.java
+++
@@ -1,9 +0,0 @@
-package com.knitting.user.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-@Mapper
-public interface UserRolesMapper {
-    int insertUserRole(@Param("userId") int userId, @Param("roleId") Long roleId);
-}
